55a4436ec9ff3b449f9fad87d208578097b6b874
Silvia C <silviachen917@gmail.com>
2015-11-03 12:07:48 -0500

    products and orders tests passed

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,10 +1,19 @@
 var mongoose = require('mongoose');
 
 var Product = require("./product");
+var Schema = mongoose.Schema;
 
 var schema = new mongoose.Schema({
-  user: { type: String, required: true },
-
+  user: {type: String, required: true},
+  item: {type: [{
+  	price: Number,
+  	productId: { type: Schema.Types.ObjectId, ref:"Product"  },
+  	quantity: Number 
+  }],
+	required: true
+	}
 })
+
+
 
 mongoose.model("Order", schema);
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,8 +1,18 @@
 var mongoose = require('mongoose');
 
 var productSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true }
+  title: { type: String, required: true, unique: true},
+  description: { type: String, required: true },
+  price: {type: Number, required: true},
+  quantity: {type: Number, required: true},
+  category: {type: [String], required: true},
+  photo: {type: String}
 })
 
+productSchema.pre('save', function(next) {
+	if (!this.photo) {
+		this.photo = 'http://fillmurray.com/200/300'
+	}
+	next();
+})
 mongoose.model("Product", productSchema);
