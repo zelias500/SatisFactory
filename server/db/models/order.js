8c1062560386afb3d103bf6cb7d664629029b775
Sunil Unka <sunil.unka@gmail.com>
2015-11-04 16:47:02 -0500

    Completed adding lineitem schema to order schema.

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,36 +2,37 @@ var mongoose = require('mongoose');
 var User = require("./user");
 var Product = require("./product");
 var Schema = mongoose.Schema;
+var _ = require('lodash')
+
+var lineItemSchema = new Schema({
+  price: Number,
+  product: { type: Schema.Types.ObjectId, ref: 'Product'},
+  quantity: Number
+})
 
 var orderSchema = new mongoose.Schema({
   user: {type: Schema.Types.ObjectId, ref: 'User', required: true}, 
-  item: {type: [{
-  	price: Number,
-  	productId: { type: Schema.Types.ObjectId, ref:"Product"  },
-  	quantity: Number
-  }],
-	required: true
-	},
+  items: [lineItemSchema],
+
   status: {type: String, enum: ['pending', 'shipping', 'completed', 'cancelled'], required: true, default: 'pending'},
-  shipTo: String,
-  billWith: String,
-  orderDate: {type: Date, default: new Date()}
+  shipTo: { type: Address },
+  billWith: { type: Billing }
+  orderDate: {type: Date, default: new Date }
+
 })
 
 orderSchema.methods.addToOrder = function(cost, id, amount) {
-	var checking = this.item.filter(function(i) {
-		if (i.productId === id && i.price === cost) {
-			return true
-		}
-		return false
-	})
-	if (checking.length) {
-		this.item[indexOf(checking[0])].quantity += amount
-	}
-	else {
-		this.item.push({price: cost, productId: id, quantity: amount})
-	}
+
+  var checking = _.find(this.item, i => (i.product.equals(id) && i.price === cost));
+  if (checking) {
+    checking.quantity += amount
+  }
+  else {
+    this.items.push({price: cost, product: id, quantity: amount})
+  }
+  this.save()
 }
+
 
 
 mongoose.model("Order", orderSchema);
