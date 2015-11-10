9ba865eac8e4a7e66bbef6f38668decdcdf5e879
Silvia C <silviachen917@gmail.com>
2015-11-10 13:06:06 -0500

    add-ons

---
+++ server/env/development.js
@@ -0,0 +1,19 @@
+module.exports = {
+ "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+ "SESSION_SECRET": "Optimus Prime is my real dad",
+ "TWITTER": {
+   "consumerKey": "gwFTauL8opraNPeCdcXH0QqDH",
+   "consumerSecret": "LMaOByPH3MeX2UvSoyCVnWc0y8ICM8FwZJSjjPadkt9p6oWV5y",
+   "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
+ },
+ // "FACEBOOK": {
+ //   "clientID": "asdfasdf",
+ //   "clientSecret": "asdfasdf",
+ //   "callbackURL": "aasdfasdfasdf"
+ // },
+ "GOOGLE": {
+   "clientID": "382724475813-2o0jeidnguthd8a6d1jkhj6bvpbk693q.apps.googleusercontent.com",
+   "clientSecret": "s5pD1oXqLRXhTfLqiuqJKXil",
+   "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
+ }
+};
