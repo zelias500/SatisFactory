cb54277a7bc872973c5e3139d317241502ea05ba
Sunil Unka <sunil.unka@gmail.com>
2015-11-10 22:25:59 -0500

    Reworking the user view, after issues at Fullstack.

---
+++ browser/js/viewuser/viewuser.controller.js
@@ -0,0 +1,6 @@
+app.controller("ViewUserCtrl", function ($scope, user, orders){
+
+  console.log("USER:", user);
+  console.log("ORDERS:", orders);
+
+})
---
+++ browser/js/viewuser/viewuser.state.js
@@ -0,0 +1,15 @@
+app.config(function ($stateProvider){
+  $stateProvider.state('viewuser', {
+    url: '/users/:id',
+    templateUrl: '/js/viewuser/viewuser.template.html',
+    controller: 'ViewUserCtrl',
+    resolve: {
+      user: function(AuthService){
+        return AuthService.getLoggedInUser();
+      },
+      orders: function(OrderFactory) {
+        return OrderFactory.getAll();
+      }
+    }
+  })
+})
